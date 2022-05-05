f342e79539334b1528e9a2daf06a6f42d967b4bc
myuon <ioi.joi.koi.loi@gmail.com>
2022-05-05 19:43:52 +0900

    chore: typescript, loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,33 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const isDev = process.env.NODE_ENV !== "production";
+const webpack = require("webpack");
 
+/** @type {webpack.Configuration} */
 module.exports = {
   mode: isDev ? "development" : "production",
   target: "web",
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
+  },
+  devtool: isDev ? "inline-source-map" : false,
+  resolve: {
+    extensions: [".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
   ],
-  devtool: isDev ? "source-map" : false,
 };
