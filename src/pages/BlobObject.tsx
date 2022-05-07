6c621d25780b9b9cc672bb617abf1a0bfdef775f
myuon <ioi.joi.koi.loi@gmail.com>
2022-05-07 19:32:16 +0900

    refactor: use ReactSyntaxHighlighter

--- src/pages/BlobObject.tsx
+++ src/pages/BlobObject.tsx
@@ -1,63 +1,18 @@
 import { useParams } from "react-router-dom";
 import { Page } from "../components/Page";
 import { assertIsDefined } from "../helper/assert";
-import React, { useEffect, useRef } from "react";
 import { useBlob } from "../api/blob";
 import { LinkButton } from "../components/Button";
-import { css } from "@emotion/react";
-import { theme } from "../components/theme";
-import hljs from "highlight.js";
-import "highlight.js/styles/github.css";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { xcode } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import React from "react";
 
-const FileViewer = ({ text }: { text: string }) => {
-  const ref = useRef<HTMLElement | null>(null);
-  useEffect(() => {
-    if (ref.current) {
-      hljs.highlightElement(ref.current);
-    }
-  }, []);
-
-  return (
-    <div
-      css={css`
-        display: grid;
-        grid-template-columns: auto 1fr;
-        gap: 8px;
-      `}
-    >
-      <div
-        css={css`
-          display: grid;
-          align-self: flex-start;
-          justify-content: flex-end;
-          padding: 0 8px;
-          background-color: ${theme.palette.gray[100]};
-        `}
-      >
-        {Array.from({ length: text.split("\n").length }, (_, k) => k + 1).map(
-          (t) => (
-            <code key={t}>{t}</code>
-          )
-        )}
-      </div>
-      <div>
-        <pre
-          css={css`
-            overflow: auto;
-          `}
-        >
-          <code
-            ref={ref}
-            css={css`
-              padding: 0 !important;
-            `}
-          >
-            {text}
-          </code>
-        </pre>
-      </div>
-    </div>
-  );
+const FileViewer = ({ text }: { text?: string }) => {
+  return text ? (
+    <SyntaxHighlighter style={xcode} language="json" showLineNumbers>
+      {text}
+    </SyntaxHighlighter>
+  ) : null;
 };
 
 export const BlobObjectPage = () => {
@@ -75,7 +30,7 @@ export const BlobObjectPage = () => {
             <LinkButton>{data.node.abbreviatedOid}</LinkButton>
           </p>
 
-          <FileViewer text={data.node.text ?? ""} />
+          <FileViewer text={data.node.text ?? undefined} />
         </div>
       ) : null}
     </Page>
